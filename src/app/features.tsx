6f35076331caf4c5fe563f7c574d91e91751802f
Anurag <anurag@xrpscan.com>
2024-09-06 01:19:41 +0530

    * Added links to footer items * Added AML/Compliance section to features

--- src/app/features.tsx
+++ src/app/features.tsx
@@ -4,14 +4,23 @@ import styles from "@/app/ui/main.module.css";
 
 const Features = () => {
     return (
-        <Row className="pb-5">
+        <Row className="pt-4 pb-5">
             <Col>
                 <div style={{width: "var(--component-highlights-item-width)", maxWidth: "300px"}} className="rt-Box lg:rt-r-position-absolute">
                     <div className="rt-Flex rt-r-ai-start rt-r-gap-2 rt-r-mb-1">
                         <CheckIcon className={styles.checkFeature} />
                         <h3 className="rt-Heading rt-r-size-3">Full history search</h3>
                     </div>
-                    <p data-accent-color="gray" className="rt-Text rt-r-size-3 rt-r-ml-6">Search within 3.4 billion transactions created since XRPL genesis ledger</p>
+                    <p data-accent-color="gray" className="rt-Text rt-r-size-3 rt-r-ml-6">Search within 3.4 billion transactions created since XRPL genesis ledger.</p>
+                </div>
+            </Col>
+            <Col>
+                <div style={{width: "var(--component-highlights-item-width)", maxWidth: "300px"}} className="rt-Box lg:rt-r-position-absolute">
+                    <div className="rt-Flex rt-r-ai-start rt-r-gap-2 rt-r-mb-1">
+                        <CheckIcon className={styles.checkFeature} />
+                        <h3 className="rt-Heading rt-r-size-3">AML / Compliance</h3>
+                    </div>
+                    <p data-accent-color="gray" className="rt-Text rt-r-size-3 rt-r-ml-6">Stay compliant, protect customers and mitigate risk. Discover new insights and find patterns.</p>
                 </div>
             </Col>
             <Col>
@@ -20,7 +29,7 @@ const Features = () => {
                         <CheckIcon className={styles.checkFeature} />
                         <h3 className="rt-Heading rt-r-size-3">Flexible queries</h3>
                     </div>
-                    <p data-accent-color="gray" className="rt-Text rt-r-size-3 rt-r-ml-6">Add search filter using 100+ fields across all supported transaction types</p>
+                    <p data-accent-color="gray" className="rt-Text rt-r-size-3 rt-r-ml-6">Add search filter using 100+ fields across all supported transaction types.</p>
                 </div>
             </Col>
             <Col>
@@ -29,7 +38,7 @@ const Features = () => {
                         <CheckIcon className={styles.checkFeature} />
                         <h3 className="rt-Heading rt-r-size-3">Instant results</h3>
                     </div>
-                    <p data-accent-color="gray" className="rt-Text rt-r-size-3 rt-r-ml-6">Blazing fast results, linked to our explorer for quick review</p>
+                    <p data-accent-color="gray" className="rt-Text rt-r-size-3 rt-r-ml-6">Blazing fast results, linked to our explorer for quick review.</p>
                 </div>
             </Col>
         </Row>
--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return(
-        <footer className="pt-4 my-md-5 pt-md-5 border-top">
+        <footer className="ml-2 pt-4 my-md-5 pt-md-5 border-top">
             <div className="row">
                 <div className="col-12 col-md">
                     <h6>XRPSCAN</h6>
@@ -11,20 +11,21 @@ const Footer = () => {
                 <div className="col-6 col-md">
                     <h6>Explore</h6>
                     <ul className="list-unstyled text-small">
-                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">XRPL Metrics</a></li>
-                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Amendments</a></li>
-                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Validator registry</a></li>
-                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">XRP Rich List</a></li>
-                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">AMM Pools</a></li>
+                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="https://xrpscan.com/metrics" target="_blank" rel="nofollow noopener">XRPL Metrics</a></li>
+                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="https://xrpscan.com/amendments" target="_blank" rel="nofollow noopener">Amendments</a></li>
+                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="https://xrpscan.com/validators" target="_blank" rel="nofollow noopener">Validator registry</a></li>
+                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="https://xrpscan.com/balances" target="_blank" rel="nofollow noopener">XRP Rich List</a></li>
+                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="https://xrpscan.com/amms" target="_blank" rel="nofollow noopener">AMM Pools</a></li>
                     </ul>
                 </div>
                 <div className="col-6 col-md">
                     <h6>Support</h6>
                     <ul className="list-unstyled text-small">
-                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">support@xrpscan.com</a></li>
-                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">API Documentation</a></li>
-                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Integration guide</a></li>
-                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Contact us</a></li>
+                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="mailto:support@xrpscan.com">support@xrpscan.com</a></li>
+                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="https://docs.xrpscan.com/" target="_blank" rel="nofollow noopener">API Documentation</a></li>
+                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="https://docs.xrpscan.com/help/integration-guide" target="_blank" rel="nofollow noopener">Integration guide</a></li>
+                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="https://docs.xrpscan.com/help/contact-us" target="_blank" rel="nofollow noopener">Contact us</a></li>
+                        <li className="mb-1"><a className="link-secondary text-decoration-none" href="https://x.com/xrpscan" target="_blank" rel="nofollow noopener">Follow us on X</a></li>
                     </ul>
                 </div>
                 {/* <div className="col-6 col-md">
